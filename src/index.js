7b1f401c881ea405e5c681c94e1a346c2eda0f28
YuliyYank <yuliy.yank@gmail.com>
2020-09-30 21:52:51 +0300

    feat: add solution of typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  var little = Infinity;
+  for (var i = 0; i < array.length; i++) {
+    var num = array[i]; 
+    var minNum = Math.min(little, num);
+    if (minNum <= little) {
+      little = minNum;
+    }
+  }
+  return minNum;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  var maxi = -Infinity;
+  for (var i = 0; i < array.length; i++) {
+    var num = array[i]; 
+    var maxNum = Math.max(maxi, num);
+    if (maxNum >= maxi) {
+      maxi = maxNum;
+    }
+  }
+  return maxNum;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  var sum = 0;
+  for (var i = 0; i < array.length; i ++) {
+    sum = sum + array[i];
+  }
+  var avge = sum / array.length;
+  return avge;
+};
